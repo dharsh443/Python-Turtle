e9d890f261e82c830b0b86b0344d3dedb02a6033
DharshVsgn <110477705+dharsh443@users.noreply.github.com>
2024-03-04 22:53:24 +0530

    Add files via upload

---
+++ Tic Tac Toe.py
@@ -0,0 +1,35 @@
+import turtle  
+sc=turtle.Screen()   
+ttl=turtle.Turtle()   
+ttl.color("violet")   
+ttl.width("3")    
+ttl.speed(4)   
+for j in range(4):  
+    ttl.forward(150)  
+    ttl.left(90)  
+ttl.penup()  
+ttl.goto(0,50)  
+ttl.pendown()  
+  
+ttl.forward(150)  
+  
+ttl.penup()  
+ttl.goto(0,100)  
+ttl.pendown()  
+  
+ttl.forward(150)  
+  
+ttl.penup()  
+ttl.goto(50,0)  
+ttl.pendown()  
+  
+ttl.left(90)  
+ttl.forward(150)  
+  
+ttl.penup()  
+ttl.goto(100,0)  
+ttl.pendown()  
+  
+ttl.forward(150)  
+  
+ttl.hideturtle()  
